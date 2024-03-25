7b528402930dd07b71d2a7d44c774736310e9160
Usuario <Usuario@CPD03-M19>
2024-03-25 12:24:47 -0300

    Atualizado 25/03/2024

--- MauiAppListaCompras/Helpers/SQLiteDatabaseHelper.cs
+++ MauiAppListaCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -40,7 +40,7 @@ namespace MauiAppListaCompras.Helpers
 
         public Task<List<Produto>> Search(string q)
         {
-            string sql = "SELECT * FROM Produto WHERE descricao LIKE '% " + q + "%'";
+            string sql = "SELECT * FROM Produto WHERE descricao LIKE '%" + q + "%'";
 
             return _conn.QueryAsync<Produto>(sql);
         }
--- MauiAppListaCompras/MainPage.xaml.cs
+++ MauiAppListaCompras/MainPage.xaml.cs
@@ -18,39 +18,34 @@ namespace MauiAppListaCompras
             string msg = $"O total é {soma:C}";
             DisplayAlert("Somatória", msg, "Fechar");
         }
-        protected override void OnAppearing()
+        protected async override void OnAppearing()
         {
             if (lista_produtos.Count == 0)
             {
-                Task.Run(async () =>
+                List<Produto> tmp = await App.Db.GetAll();
+                foreach (Produto p in tmp)
                 {
-                    List<Produto> tmp = await App.Db.GetAll();
-                    foreach (Produto p in tmp)
-                    {
-                        lista_produtos.Add(p);
-                    }
-                });// Fecha a Task
+                    lista_produtos.Add(p);
+                }
             }// Fecha if
         }
 
         private async void ToolbarItem_Add(object sender, EventArgs e)
         {
-            await Shell.Current.GoToAsync("//NovoProduto");
-
+            await Navigation.PushAsync(new Views.NovoProduto());
         }
 
-        private void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+        private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
         {
             string q = e.NewTextValue;
             lista_produtos.Clear();
-            Task.Run(async () =>
+            
+            List<Produto> tmp = await App.Db.Search(q);
+            foreach (Produto p in tmp)
             {
-                List<Produto> tmp = await App.Db.Search(q);
-                foreach (Produto p in tmp)
-                {
-                    lista_produtos.Add(p);
-                }
-            });// Fecha Task
+                lista_produtos.Add(p);
+            }
+            
         }// Fecha método do evento
 
         private void ref_carregando_Refreshing(object sender, EventArgs e)
@@ -69,7 +64,12 @@ namespace MauiAppListaCompras
 
         private void lst_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
+            Produto? p = e.SelectedItem as Produto;
 
+            Navigation.PushAsync(new Views.EditarProduto
+            {
+                BindingContext = p
+            });
         }
 
         private async void MenuItem_Clicked_Remover(object sender, EventArgs e)
@@ -85,6 +85,7 @@ namespace MauiAppListaCompras
                 {
                     await App.Db.Delete(p.Id);
                     await DisplayAlert("Sucesso!", "Produto Removido", "OK");
+                    lista_produtos.Remove(p);
                 }
             } catch(Exception ex)
             {
